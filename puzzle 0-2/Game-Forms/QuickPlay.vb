8acdb7303fdb7598ffdc0dfbaf353444a591099c
untitledcade <priva@DESKTOP-89RB5D3>
2024-08-06 22:16:45 -1000

    changed win credit thing

--- puzzle 0-2/Game-Forms/QuickPlay.vb
+++ puzzle 0-2/Game-Forms/QuickPlay.vb
@@ -12,26 +12,26 @@
     End Function
 
     Private Sub BabyModeButton_Click(sender As Object, e As EventArgs) Handles BabyModeButton.Click
-        StartGame(1, 15, 25, 35, 25, 0)
+        StartGame(1, 15, 25, 35, 25, 0.5)
     End Sub
 
     Private Sub NormalButton_Click(sender As Object, e As EventArgs) Handles NormalButton.Click
-        StartGame(1, 9, 15, 25, 50, 1)
+        StartGame(1, 9, 15, 25, 50, 5)
     End Sub
 
     Private Sub ModerateButton_Click(sender As Object, e As EventArgs) Handles ModerateButton.Click
-        StartGame(1, 5, 10, 15, 100, 5)
+        StartGame(1, 5, 10, 15, 100, 10)
     End Sub
 
     Private Sub HardButton_Click(sender As Object, e As EventArgs) Handles HardButton.Click
-        StartGame(2, 3, 6, 9, 200, 10)
+        StartGame(2, 3, 6, 9, 200, 20)
     End Sub
 
     Private Sub GamerButton_Click(sender As Object, e As EventArgs) Handles GamerButton.Click
-        StartGame(1, 1, 3, 5, 400, 20)
+        StartGame(1, 1, 3, 5, 500, 50)
     End Sub
 
     Private Sub MarvButton_Click(sender As Object, e As EventArgs) Handles MarvButton.Click
-        StartGame(5, 5, 10, 15, 600, 50)
+        StartGame(5, 5, 10, 15, 1000, 100)
     End Sub
 End Class
