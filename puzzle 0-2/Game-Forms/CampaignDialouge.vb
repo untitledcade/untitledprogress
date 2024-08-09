bd1350b30ea8b7c3819918328d73b45dbe0465ad
untitledcade <priva@DESKTOP-89RB5D3>
2024-08-09 01:27:18 -1000

    Changed XPConverter odds to be well... less lucky also fixed code reguarding XPConverter and Dialouge

--- puzzle 0-2/Game-Forms/CampaignDialouge.vb
+++ puzzle 0-2/Game-Forms/CampaignDialouge.vb
@@ -47,7 +47,9 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Form1.Show()
+        If StartGame = True Then
+            Form1.Show()
+        End If
         Me.Close()
     End Sub
 
--- puzzle 0-2/Game-Forms/XPConverter.vb
+++ puzzle 0-2/Game-Forms/XPConverter.vb
@@ -12,6 +12,7 @@ Public Class XPConverter
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         XPBetted = NumericUpDown1.Value
         If XPBetted = 0 Then
+            CampaignDialouge.Show()
             CampaignDialouge.CampaignDialouge("The Wheel of Odds", "Yeaaaa you can bet nothing. Please put at least 1 XP there.", 25)
         ElseIf My.Settings.userXP < XPBetted Then
             Campaign.StartGameDialouge(0, 3, 5, 15, 30, 600, 50, "The Wheel of Odds", "You think your slick? You think you can finess me? HELL NAH.", 25)
@@ -21,23 +22,25 @@ Public Class XPConverter
             My.Settings.Save()
             Dim rand As New Random()
 
-            Dim actionWheel As Integer = rand.Next(1, 6)
+            Dim actionWheel As Integer = rand.Next(1, 4)
             Dim amountWheel As Integer = rand.Next(1, XPBetted)
 
-            If actionWheel = 1 Or 3 Then
+            If actionWheel = 1 Then
                 My.Settings.userCredit = My.Settings.userCredit + amountWheel
                 WheelText.Text = "Earned " & amountWheel & " BugCoin!"
-            ElseIf actionWheel = 2 Or 4 Then
+            ElseIf actionWheel = 3 Then
                 My.Settings.userXP = My.Settings.userXP + amountWheel
                 WheelText.Text = "Earned " & amountWheel & " XP!"
-            ElseIf actionWheel = 5 Then
+            ElseIf actionWheel = 2 Then
                 My.Settings.userCredit = My.Settings.userCredit - amountWheel
                 WheelText.Text = "Lost " & amountWheel & " BugCoin!"
-            ElseIf actionWheel = 6 Then
+            ElseIf actionWheel = 4 Then
                 My.Settings.userXP = My.Settings.userXP - amountWheel
                 WheelText.Text = "Lost " & amountWheel & " XP!"
             End If
 
+            actionWheel = 0
+            amountWheel = 0
             NumericUpDown1.Value = 0
             UpdateCounters()
 
